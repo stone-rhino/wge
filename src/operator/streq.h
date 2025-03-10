8c26d13a9e9b92c22341893dc9074f6330a74daa
zhouyujt <zhouyu-jt@163.com>
2025-03-10 15:25:12 +0800

    fix(operator): fix streq was not macro expanded

--- src/operator/streq.h
+++ src/operator/streq.h
@@ -4,6 +4,10 @@
 
 namespace SrSecurity {
 namespace Operator {
+/**
+ * Performs a string comparison and returns true if the parameter string is identical to the input
+ * string. Macro expansion is performed on the parameter string before comparison.
+ */
 class Streq : public OperatorBase {
   DECLARE_OPERATOR_NAME(streq);
 
@@ -16,7 +20,12 @@ public:
 public:
   bool evaluate(Transaction& t, const Common::Variant& operand) const override {
     if (IS_STRING_VIEW_VARIANT(operand)) [[likely]] {
-      return is_not_ ^ (literal_value_ == std::get<std::string_view>(operand));
+      if (!macro_) [[likely]] {
+        return is_not_ ^ (literal_value_ == std::get<std::string_view>(operand));
+      } else {
+        MACRO_EXPAND_STRING_VIEW(macro_value);
+        return is_not_ ^ (macro_value == std::get<std::string_view>(operand));
+      }
     } else {
       return false;
     }
