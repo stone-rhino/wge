8cfd30c98ed9e19d176bdb3777e7cde1e5e325b6
zhouyujt <zhouyu-jt@163.com>
2025-03-10 09:24:36 +0800

    feat(variable): add REQUEST_METHOD variable

--- src/variable/request_method.h
+++ src/variable/request_method.h
@@ -12,7 +12,10 @@ public:
       : VariableBase(std::move(sub_name), is_not, is_counter) {}
 
 public:
-  const Common::Variant& evaluate(Transaction& t) const override { assert(false); throw "Not implemented!"; };
+  const Common::Variant& evaluate(Transaction& t) const override {
+    return t.getEvaluatedBuffer(Transaction::EvaluatedBufferType::Variable)
+        .set(t.getUriInfo().method_);
+  };
 };
 } // namespace Variable
 } // namespace SrSecurity
