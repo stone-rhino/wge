d17bc290f2e3eb27d63c54f168be30fe58ffef83
zhouyujt <zhouyu-jt@163.com>
2025-03-12 12:04:11 +0800

    feat(common): add hash_table and lru_cache

---
+++ src/common/hash_table.hpp
@@ -0,0 +1,322 @@
+#pragma once
+#include <forward_list>
+#include <functional>
+#include <memory>
+#include <shared_mutex>
+#include <vector>
+
+namespace SrSecurity {
+namespace Common {
+
+/**
+ * Implement a simple hash table that provides the functionality of a general hash table and
+ * provides locking by slot. Locking by slot is mainly used for multi-threaded synchronization,
+ * to reduce the granularity of the lock of the hash table, and to split a large lock of the entire
+ * hash table into N small locks to reduce lock contention and improve concurrency performance.
+ * Note:
+ * This class does not support rehash rearrangement function, using a fixed prime number (8191) as
+ * the number of slots, we can adjust this value according to the business, but to reduce hash
+ * collisions, it must be a prime number.
+ * @tparam KEY the type of key
+ * @tparam VALUE the type of value
+ * @tparam slot_count the count of hash table slot, must be a prime number
+ */
+template <typename KEY, typename VALUE, size_t slot_count = 8191> class HashTable {
+public:
+  HashTable(std::function<size_t(const KEY&)> hash_func = nullptr,
+            size_t per_locker = 8 // Each locker manages 8 slots by default
+            )
+      : slots_(slot_count),
+        per_locker_(per_locker == 0 ? 1 : (per_locker > slot_count ? slot_count : per_locker)),
+        hash_func_(hash_func) {
+    static_assert(!std::is_same<KEY, size_t>::value, "KEY cannot be size_t");
+    size_t locker_count = caclLockerCount(per_locker_);
+    lockers_.reserve(locker_count);
+    for (size_t i = 0; i < locker_count; i++) {
+      lockers_.push_back(std::make_unique<std::shared_mutex>());
+    }
+  }
+
+public:
+  struct Node {
+    Node(const KEY& k, const VALUE& v) : key(k), value(v) {}
+    KEY key;
+    VALUE value;
+  };
+
+  // Provide standard library style interface
+public:
+  class iterator {
+    friend class HashTable;
+
+  public:
+    iterator() : slot_index_(size_t(-1)) {}
+    iterator(const iterator& iter) {
+      slot_index_ = iter.slot_index_;
+      node_iter_ = iter.node_iter_;
+    }
+
+  public:
+    using list_iterator = typename std::forward_list<Node>::iterator;
+
+    // Overload operators
+  public:
+    bool operator==(const iterator& iter) const {
+      return slot_index_ == iter.slot_index_ && node_iter_ == iter.node_iter_;
+    }
+    bool operator!=(const iterator& iter) const { return !(*this == iter); }
+    const list_iterator operator->() const { return node_iter_; }
+    const list_iterator operator*() const { return node_iter_; }
+    list_iterator operator->() { return node_iter_; }
+    list_iterator operator*() { return node_iter_; }
+    iterator& operator=(const iterator& iter) {
+      slot_index_ = iter.slot_index_;
+      node_iter_ = iter.node_iter_;
+      return *this;
+    }
+    iterator& operator++() {
+      ++node_iter_;
+      return *this;
+    }
+
+  public:
+    size_t slotIndex() const { return slot_index_; }
+    list_iterator nodeIter() const { return node_iter_; }
+
+  private:
+    size_t slot_index_;
+    list_iterator node_iter_;
+  };
+
+  /**
+   * Find the node
+   * @param key the key of the node
+   * @return iterator the iterator of the node
+   */
+  iterator find(const KEY& key) { return internalFind(getSlotIndex(key), key); }
+
+  /**
+   * Insert a node
+   * @param key the key of the node
+   * @param value the value of the node
+   * @return iterator the iterator of the inserted node
+   */
+  iterator insert(const KEY& key, const VALUE& value) {
+    size_t index = getSlotIndex(key);
+    iterator iter = internalFind(index, key);
+    if (iter != end(key)) {
+      return iter;
+    }
+
+    slots_[index].emplace_front(key, value);
+    iter.slot_index_ = index;
+    iter.node_iter_ = slots_[index].begin();
+
+    return iter;
+  }
+
+  /**
+   * Insert a node
+   * @param key the key of the node
+   * @param value the value of the node
+   * @return iterator the iterator of the inserted node
+   */
+  iterator insert(KEY&& key, VALUE&& value) {
+    size_t index = getSlotIndex(key);
+    iterator iter = internalFind(index, key);
+    if (iter != end(key)) {
+      return iter;
+    }
+
+    slots_[index].emplace_front(std::forward<KEY>(key), std::forward<VALUE>(value));
+    iter.slot_index_ = index;
+    iter.node_iter_ = slots_[index].begin();
+
+    return iter;
+  }
+
+  /**
+   * Erase the node
+   * @param iter the iterator of the node that needs to be deleted
+   */
+  void erase(iterator iter) {
+    if (iter.slot_index_ != size_t(-1)) {
+      std::forward_list<Node>& list = slots_[iter.slot_index_];
+      if (iter.nodeIter() != list.end()) {
+        if (list.begin() == iter.nodeIter()) {
+          list.pop_front();
+        } else {
+          for (auto nodeIter = list.begin(); nodeIter != list.end(); ++nodeIter) {
+            auto next = nodeIter;
+            ++next;
+            if (next != list.end() && next == iter.nodeIter()) {
+              list.erase_after(nodeIter);
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Get the begin iterator of the specified key
+   * @param key the specified key
+   * @return iterator the begin iterator of the specified key
+   */
+  iterator begin(const KEY& key) {
+    iterator iter;
+    iter.slot_index_ = getSlotIndex(key);
+    iter.node_iter_ = slots_[iter.slot_index_].begin();
+
+    return iter;
+  }
+
+  /**
+   * Get the end iterator of the specified key
+   * @param key the specified key
+   * @return iterator the end iterator of the specified key
+   */
+  iterator end(const KEY& key) {
+    iterator iter;
+    iter.slot_index_ = getSlotIndex(key);
+    iter.node_iter_ = slots_[iter.slot_index_].end();
+
+    return iter;
+  }
+
+  void swap(HashTable& table) {
+    slots_.swap(table.slots_);
+    lockers_.swap(table.lockers_);
+    std::swap(per_locker_, table.per_locker_);
+  }
+
+  // Provide lock/unlock interface
+public:
+  /**
+   * Lock the read lock of the group
+   * @param key the key of the node
+   */
+  void readLock(const KEY& key) { lockers_[getLockerIndex(getSlotIndex(key))]->lock_shared(); }
+  void readLock(size_t index) {
+    if (index < slots_.size()) {
+      lockers_[getLockerIndex(index)]->lock_shared();
+    }
+  }
+  bool tryReadLock(const KEY& key) {
+    return lockers_[getLockerIndex(getSlotIndex(key))]->try_lock_shared();
+  }
+  bool tryReadLock(size_t index) {
+    if (index < slots_.size()) {
+      return lockers_[getLockerIndex(index)]->try_lock_shared();
+    }
+
+    return false;
+  }
+
+  /**
+   * Unlock the read lock of the group
+   * @param key the key of the node
+   */
+  void readUnlock(const KEY& key) { lockers_[getLockerIndex(getSlotIndex(key))]->unlock_shared(); }
+  void readUnlock(size_t index) {
+    if (index < slots_.size()) {
+      lockers_[getLockerIndex(index)]->unlock_shared();
+    }
+  }
+
+  /**
+   * Lock the write lock of the group
+   * @param key the key of the node
+   */
+  void writeLock(const KEY& key) { lockers_[getLockerIndex(getSlotIndex(key))]->lock(); }
+  void writeLock(size_t index) {
+    if (index < slots_.size()) {
+      lockers_[getLockerIndex(index)]->lock();
+    }
+  }
+  bool tryWriteLock(const KEY& key) {
+    return lockers_[getLockerIndex(getSlotIndex(key))]->try_lock();
+  }
+  bool tryWriteLock(size_t index) {
+    if (index < slots_.size()) {
+      return lockers_[getLockerIndex(index)]->try_lock();
+    }
+
+    return false;
+  }
+
+  /**
+   * Unlock the write lock of the group
+   * @param key the key of the node
+   */
+  void writeUnlock(const KEY& key) { lockers_[getLockerIndex(getSlotIndex(key))]->unlock(); }
+  void writeUnlock(size_t index) {
+    if (index < slots_.size()) {
+      lockers_[getLockerIndex(index)]->unlock();
+    }
+  }
+
+private:
+  std::vector<std::forward_list<Node>> slots_;
+  std::vector<std::unique_ptr<std::shared_mutex>> lockers_;
+  size_t per_locker_;
+  std::function<size_t(const KEY&)> hash_func_;
+
+private:
+  /**
+   * Get the group index according to the key
+   * @param key the key of the node
+   * @return size_t the group index
+   */
+  size_t getSlotIndex(const KEY& key) const {
+    if (hash_func_) {
+      return hash_func_(key) % slots_.size();
+    } else {
+      return std::hash<KEY>()(key) % slots_.size();
+    }
+  }
+
+  /**
+   * Calculate how many locks are needed
+   * @param slot_count the count of slots
+   * @param per_locker  how many slots per lock
+   * @return size_t the count of locks
+   */
+  size_t caclLockerCount(size_t per_locker) const {
+    if (slot_count % per_locker == 0) {
+      return slot_count / per_locker;
+    } else {
+      return (slot_count / per_locker) + 1;
+    }
+  }
+
+  /**
+   * Get the lock index of the corresponding slot
+   * @param slot_index the index of the slot
+   * @return size_t the lock index
+   */
+  size_t getLockerIndex(size_t slot_index) const { return slot_index / per_locker_; }
+
+  /**
+   * Find the node
+   * @param slot_index the index of the slot
+   * @param key the key of the node
+   * @return iterator the iterator of the node
+   */
+  iterator internalFind(size_t slot_index, const KEY& key) const {
+    iterator findIter;
+    std::forward_list<Node>& list = const_cast<std::forward_list<Node>&>(slots_[slot_index]);
+    findIter.slot_index_ = slot_index;
+    findIter.node_iter_ = list.end();
+    for (auto iter = list.begin(); iter != list.end(); ++iter) {
+      if (iter->key == key) {
+        findIter.node_iter_ = iter;
+        break;
+      }
+    }
+    return findIter;
+  }
+};
+} // namespace Common
+} // namespace SrSecurity
---
+++ src/common/lru_cache.hpp
@@ -0,0 +1,232 @@
+#pragma once
+
+#include <array>
+#include <functional>
+#include <list>
+#include <mutex>
+
+#include "hash_table.hpp"
+
+namespace SrSecurity {
+namespace Common {
+/**
+ * Use LRU(Least Recently Used) algorithm to clean the cache.
+ * All methods of this class are thread-safe.
+ * @tparam KEY the type of key
+ * @tparam VALUE the type of value
+ * @tparam hash_table_slot_count the count of hash table slot, must be a prime number
+ */
+template <typename KEY, typename VALUE, size_t hash_table_slot_count = 8191> class LruCache {
+public:
+  LruCache(size_t max_size = 1024 * 16) : max_size_(max_size > 0 ? max_size : 1) {}
+  LruCache(const LruCache& cache) = delete;
+
+private:
+  struct Node;
+  using HashTableType = HashTable<KEY, typename std::list<Node>::iterator, hash_table_slot_count>;
+
+public:
+  /**
+   * Get the cache of the specified key and process the node. If the cache of the specified key does
+   * not exist, add it. After calling this method, the corresponding node is moved to the cache
+   * head.
+   * @param key the specified key
+   * @param found the operation to be performed after finding (adding) the cache of the specified
+   * key.
+   * @param value_factory_cb when the cache of the specified key does not exist, use this factory
+   * function to generate a new node.
+   */
+  void access(const KEY& key, std::function<void(VALUE&)> found,
+              std::function<VALUE()> value_factory_cb) {
+    map_.readLock(key);
+
+    // Find the node and process it
+    if (lookup(key, found)) {
+      map_.readUnlock(key);
+      return;
+    }
+
+    if (!value_factory_cb) {
+      return;
+    }
+
+    // Upgrade to write lock, this operation is not atomic, need to verify again whether it exists
+    map_.readUnlock(key);
+    map_.writeLock(key);
+    if (lookup(key, found)) {
+      map_.writeUnlock(key);
+      return;
+    }
+
+    // Add node and process
+    mutex_.lock();
+    Node node(value_factory_cb());
+    cache_.emplace_front(std::move(node));
+    auto cache_iter = cache_.begin();
+    cache_iter->table_iter = map_.insert(key, cache_iter);
+    if (found) {
+      found(cache_iter->value);
+    }
+    mutex_.unlock();
+
+    map_.writeUnlock(key);
+
+    tryClean(1);
+  }
+
+  /**
+   * Peek at the cache of the specified key and process the node.
+   * After calling this method, the cache will not be changed: if the node does not exist, a new
+   * node will not be added; if the node exists, it will not be moved to the cache head.
+   * @param key the specified key
+   * @param found the operation to be performed after finding (adding) the cache of the specified
+   * key.
+   */
+  void peek(const KEY& key, std::function<void(const VALUE*)> found) {
+    if (!found) {
+      return;
+    }
+
+    map_.readLock(key);
+
+    auto iter = map_.find(key);
+    if (iter != map_.end(key)) {
+      mutex_.lock();
+      found(&(iter->value->value));
+      mutex_.unlock();
+    } else {
+      found(nullptr);
+    }
+
+    map_.readUnlock(key);
+  }
+
+  /**
+   * Peek at the cache of the specified key and process the node.
+   * After calling this method, the cache will not be changed: if the node does not exist, a new
+   * node will not be added; if the node exists, it will not be moved to the cache head.
+   * @param pos the position of the cache node to be peeked. base 0
+   * @param found the operation to be performed after finding (adding) the cache of the specified
+   * key. if the node does not exist, const VALUE* == nullptr
+   */
+  void peek(size_t pos, std::function<void(const VALUE*)> found) {
+    if (!found) {
+      return;
+    }
+
+    mutex_.lock();
+
+    auto iter = cache_.begin();
+    for (size_t i = 0; i < pos; i++) {
+      ++iter;
+    }
+
+    if (iter != cache_.end()) {
+      found(&(iter->value));
+    } else {
+      found(nullptr);
+    }
+
+    mutex_.unlock();
+  }
+
+  /**
+   * Clear the cache.
+   * @param count the count of nodes to be cleaned
+   * @param predicat the condition for cleaning nodes. return false to stop cleaning, otherwise
+   * continue cleaning.
+   */
+  void clean(size_t count, std::function<bool(const VALUE& value)> predicat) {
+    mutex_.lock();
+
+    for (size_t i = 0; i < count; ++i) {
+      if (cache_.empty()) {
+        break;
+      }
+
+      if (!predicat(cache_.back().value)) {
+        break;
+      }
+
+      const size_t slot_index = cache_.back().table_iter.slotIndex();
+      if (!map_.tryWriteLock(slot_index)) {
+        break;
+      }
+
+      map_.erase(cache_.rbegin()->table_iter);
+      cache_.pop_back();
+      map_.writeUnlock(slot_index);
+    }
+
+    mutex_.unlock();
+  }
+
+  /**
+   * Get the size of the cache.
+   * @return the size of the cache
+   */
+  size_t size() {
+    std::lock_guard<std::mutex> locker(mutex_);
+    return cache_.size();
+  }
+
+  size_t maxSize() const { return max_size_; }
+
+private:
+  // The type of cache node
+  struct Node {
+    Node(VALUE&& v) : value(std::forward<VALUE>(v)) {}
+
+    typename HashTableType::iterator table_iter;
+    VALUE value;
+  };
+
+  std::list<Node> cache_;
+  HashTableType map_;
+  std::mutex mutex_;
+  size_t max_size_;
+
+private:
+  // Find the node and process it
+  bool lookup(const KEY& key, std::function<void(VALUE&)> found) {
+    auto iter = map_.find(key);
+    if (iter != map_.end(key)) {
+      mutex_.lock();
+      cache_.splice(cache_.begin(), cache_, iter->value);
+      if (found) {
+        found(iter->value->value);
+      }
+      mutex_.unlock();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  // Clean the cache, only called after adding a node
+  void tryClean(size_t count) {
+    if (cache_.size() <= max_size_) {
+      return;
+    }
+
+    mutex_.lock();
+
+    if (cache_.size() > max_size_) {
+      for (size_t i = 0; i < count; ++i) {
+        const size_t slot_index = cache_.back().table_iter.slotIndex();
+        if (!map_.tryWriteLock(slot_index)) {
+          break;
+        }
+
+        map_.erase(cache_.rbegin()->table_iter);
+        cache_.pop_back();
+        map_.writeUnlock(slot_index);
+      }
+    }
+
+    mutex_.unlock();
+  }
+};
+} // namespace Common
+} // namespace SrSecurity
