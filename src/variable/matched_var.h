1840cbc76f3cd15588d0b88f2c2c60ed3712a9c3
zhouyu <zhouyu-jt@163.com>
2025-03-13 01:25:10 +0800

    feat(variable): add MATCHED_VAR

--- src/variable/matched_var.h
+++ src/variable/matched_var.h
@@ -12,7 +12,13 @@ public:
       : VariableBase(std::move(sub_name), is_not, is_counter) {}
 
 public:
-  void evaluate(Transaction& t, Common::EvaluateResult& result) const override { assert(false); throw "Not implemented!"; };
+  void evaluate(Transaction& t, Common::EvaluateResult& result) const override {
+    if (!is_counter_) [[likely]] {
+      result.append(t.getMatched(0));
+    } else {
+      result.append(IS_EMPTY_VARIANT(t.getMatched(0)) ? 0 : 1);
+    }
+  };
 };
 } // namespace Variable
 } // namespace SrSecurity
