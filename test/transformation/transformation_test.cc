23c2d5f3f8dc52cabee97b40a64d4a2137927dcf
zhouyu <zhouyu-jt@163.com>
2025-03-21 15:07:07 +0800

    feat(test): add transformation test framework

---
+++ test/transformation/transformation_test.cc
@@ -0,0 +1,157 @@
+#include <gtest/gtest.h>
+
+#include "transformation/transform_include.h"
+
+namespace SrSecurity {
+namespace Transformation {
+class TransformationTest : public ::testing::Test {};
+
+TEST_F(TransformationTest, base64DecodeExt) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, base64Decode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, base64Encode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, cmdLine) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, compressWhiteSpace) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, cssDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, escapeSeqDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, hexDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, hexEncode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, htmlEntityDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, jsDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, length) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, lowercase) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, md5) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, normalisePathWin) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, normalisePath) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, normalizePathWin) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, normalizePath) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, parityEven7Bit) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, parityOdd7Bit) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, ParityZero7Bit) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, removeComments) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, removeCommentChar) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, removeNulls) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, removeWhitespace) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, replaceComments) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, replaceNulls) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, sha1) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, sqlHexDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, trimLeft) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, trimRight) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, trim) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, upperCase) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, urlDecodeUni) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, urlDecode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, urlEncode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+
+TEST_F(TransformationTest, utf8ToUnicode) {
+  // TODO(zhouyu 2025-03-21): Implement this test
+}
+} // namespace Transformation
+} // namespace SrSecurity
