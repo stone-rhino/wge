cb9b0b46a8b71318f9b835dbc819d4d75d1bd2f2
zhouyujt <zhouyu-jt@163.com>
2025-03-26 17:30:24 +0800

    feat(variable): add REQUEST_COOKIES_NAMES

--- src/variable/request_cookies_names.h
+++ src/variable/request_cookies_names.h
@@ -14,8 +14,29 @@ public:
 
 public:
   void evaluate(Transaction& t, Common::EvaluateResults& result) const override {
-    assert(false);
-    throw "Not implemented!";
+    const std::unordered_map<std::string_view, std::string_view>& cookies = t.getCookies();
+    if (cookies.empty()) [[unlikely]] {
+      return;
+    }
+
+    if (!is_counter_) [[likely]] {
+      if (sub_name_.empty()) {
+        for (const auto& [key, value] : cookies) {
+          if (!hasExceptVariable(key)) [[likely]] {
+            result.append(key);
+          }
+        }
+      } else {
+        auto iter = cookies.find(sub_name_);
+        if (iter != cookies.end()) {
+          if (!hasExceptVariable(sub_name_)) [[likely]] {
+            result.append(iter->first);
+          }
+        }
+      }
+    } else {
+      result.append(static_cast<int>(cookies.size()));
+    }
   };
 
   bool isCollection() const override { return sub_name_.empty(); };
