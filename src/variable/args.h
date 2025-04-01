d8262b401caebafb140621585e87826942816f2e
zhouyu <zhouyu-jt@163.com>
2025-04-01 10:07:19 +0800

    fix(variable): Fix Args and ArgsNames variable

--- src/variable/args.h
+++ src/variable/args.h
@@ -19,10 +19,67 @@ public:
 
 public:
   void evaluate(Transaction& t, Common::EvaluateResults& result) const override {
-    ArgsGet args_get(std::string(sub_name_), is_not_, is_counter_);
-    args_get.evaluate(t, result);
-    ArgsPost args_post(std::string(sub_name_), is_not_, is_counter_);
-    args_post.evaluate(t, result);
+    auto& line_query_params = t.getRequestLineInfo().query_params_.getLinked();
+    auto& line_query_params_map = t.getRequestLineInfo().query_params_.get();
+    auto& body_query_params = t.getBodyQueryParam().getLinked();
+    auto& body_query_params_map = t.getBodyQueryParam().get();
+
+    RETURN_IF_COUNTER(
+        // collection
+        { result.append(static_cast<int>(line_query_params.size() + body_query_params.size())); },
+        // specify subname
+        {
+          int count = line_query_params_map.find(sub_name_) != line_query_params_map.end() ? 1 : 0;
+          if (body_query_params_map.find(sub_name_) != body_query_params_map.end()) {
+            count++;
+          }
+          result.append(count);
+        });
+
+    RETURN_VALUE(
+        // collection
+        {
+          for (auto& elem : line_query_params) {
+            if (!hasExceptVariable(elem->first)) [[likely]] {
+              result.append(elem->second);
+            }
+          }
+          for (auto& elem : body_query_params) {
+            if (!hasExceptVariable(elem->first)) [[likely]] {
+              result.append(elem->second);
+            }
+          }
+        },
+        // collection regex
+        {
+          for (auto& elem : line_query_params) {
+            if (!hasExceptVariable(elem->first)) [[likely]] {
+              if (match(elem->first)) {
+                result.append(elem->second);
+              }
+            }
+          }
+          for (auto& elem : body_query_params) {
+            if (!hasExceptVariable(elem->first)) [[likely]] {
+              if (match(elem->first)) {
+                result.append(elem->second);
+              }
+            }
+          }
+        },
+        // specify subname
+        {
+          if (!hasExceptVariable(sub_name_)) [[likely]] {
+            auto iter = line_query_params_map.find(sub_name_);
+            if (iter != line_query_params_map.end()) {
+              result.append(iter->second);
+            }
+            auto iter2 = body_query_params_map.find(sub_name_);
+            if (iter2 != body_query_params_map.end()) {
+              result.append(iter2->second);
+            }
+          }
+        });
   };
   bool isCollection() const override { return sub_name_.empty(); };
 };
